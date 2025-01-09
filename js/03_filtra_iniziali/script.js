881c5e0765843f2485aa1d1079553e25728c0ccb
marco-gulino <marcogulino98@gmail.com>
2025-01-09 16:38:34 +0100

    [feat] complete third exercise

--- js/03_filtra_iniziali/script.js
+++ js/03_filtra_iniziali/script.js
@@ -1,13 +1,17 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let letter = prompt("Inserisci la lettera iniziale con cui filtrare la lista di nomi").toUpperCase();
 
 // Dichiara la funzione qui.
-
+function searchInitials(initial, words) {
+    //Array temporaneo
+    const tempArr = words.filter((word) => word[0].startsWith(initial));
+    return tempArr;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const filteredNames = searchInitials(letter, names);
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
