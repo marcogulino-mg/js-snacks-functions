785805613245b457292df8152e26ad8e67616e5a
marco-gulino <marcogulino98@gmail.com>
2025-01-09 17:08:51 +0100

    [feat] fourth exercise complete

--- js/04_conta_vocali/script.js
+++ js/04_conta_vocali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+let word = prompt("Inserisci una parola");
 
 
 // Dichiara la funzione qui.
+function vowelsNum(text) {
+    //Creo un array riempito con le lettere che rispettano le condizioni di .match()
+    const tempArr = text.match(/[aeiou]/gi);
 
+    let count = tempArr.length + " " + tempArr;
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vowelsNum(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
